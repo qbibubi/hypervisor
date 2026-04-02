eba800d0a25d1a86469e0a68cd9bdc4113d49bfe
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2026-04-02 06:32:03 +0200

    update(driver): clean up, switch to neovim development

--- driver/hypervisor/drvmain.cpp
+++ driver/hypervisor/drvmain.cpp
@@ -1,18 +1,6 @@
 #include <wdm.h>
 
-<<<<<<< HEAD
-// example
-
-NTSTATUS DriverEntry(PDRIVER_OBJECT, PUNICODE_STRING) { return STATUS_SUCCESS; }
-=======
-/**
-* 1) Driver install script for Windows
-* 2) How to place the driver in proper place under HyperV
-* 3) 
-*/
-
 NTSTATUS DriverEntry(PDRIVER_OBJECT, PUNICODE_STRING)
 {
-	return STATUS_SUCCESS;
+    return STATUS_SUCCESS;
 }
->>>>>>> 885a7fd9af744c27c45d710185979a5d11a8ddac
