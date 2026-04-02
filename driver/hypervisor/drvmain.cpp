793298356be2c3b2d3e4dacdcd595721801013cd
Jakub Sobieraj <jakub.sobieraj2137@gmail.com>
2026-04-02 03:59:11 +0200

    update: .gitignore

--- driver/hypervisor/drvmain.cpp
+++ driver/hypervisor/drvmain.cpp
@@ -1,6 +1,5 @@
 #include <wdm.h>
 
-NTSTATUS DriverEntry(PDRIVER_OBJECT, PUNICODE_STRING)
-{
-	return STATUS_SUCCESS;
-}
+// example
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT, PUNICODE_STRING) { return STATUS_SUCCESS; }
