885a7fd9af744c27c45d710185979a5d11a8ddac
qbibubi <jakub.sobieraj2137@gmail.com>
2026-04-02 04:03:46 -0700

    update(driver): starting development

--- driver/hypervisor/drvmain.cpp
+++ driver/hypervisor/drvmain.cpp
@@ -1,5 +1,11 @@
 #include <wdm.h>
 
+/**
+* 1) Driver install script for Windows
+* 2) How to place the driver in proper place under HyperV
+* 3) 
+*/
+
 NTSTATUS DriverEntry(PDRIVER_OBJECT, PUNICODE_STRING)
 {
 	return STATUS_SUCCESS;
